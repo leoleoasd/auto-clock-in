2e0a8f35ed570236e29adf4aa4a5360862327056
Leo Lu <luyuxuanleo@gmail.com>
2021-12-07 09:47:42 +0800

    更新

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ EMAIL_SERVER = environ['EMAIL_SERVER']
 EMAIL_PORT = int(environ['EMAIL_PORT'])
 core = json.loads(environ['DATA'])
 
-URL_SESSION = 'http://xgxt.bjut.edu.cn/nonlogin/qywx/authentication.htm?appId=402880c97b1aa5f7017b1ad2bd97001b&urlb64=L3dlaXhpbi9zYW55dGgvaG9tZS5odG1s'
+
+result = ""
+
+URL_SESSION = 'http://xgxt.bjut.edu.cn/nonlogin/qywx/authentication.htm?appId=2c95de297d4f8bfa017d8631748b7fe2&urlb64=L3dlYkFwcC94dWVnb25nL2luZGV4Lmh0bWwjL2FjdGlvbi9iYXNlSW5kZXgvJUU3JUE3JUJCJUU1JThBJUE4JUU1JUFEJUE2JUU1JUI3JUE1'
 URL_CLOCKIN = 'http://xgxt.bjut.edu.cn/syt/zzapply/operation.htm'
 
 # Part1 Get session
@@ -27,16 +30,22 @@ HEADER_SESSION = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
     'Connection': 'keep-alive',
-    'Host': 'bjut.sanyth.com:81',
-    'Cookie': 'id='+core['id']+'; token='+core['token'],
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/85.0.4183.83 Safari/537.36 '
+    'X-Requested-With': 'com.tencent.wework',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; GM1910 Build/RKQ1.201022.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 wxwork/3.1.7 MicroMessenger/7.0.1 NetType/4G Language/zh Lang/zh'
 }
-s = requests.Session()
-response = s.get(URL_SESSION, headers=HEADER_SESSION)
-session = response.history[0].headers['Set-Cookie'].split(';')[0]
-cookie = 'id=' + core['id'] + '; token=' + core['token'] + '; ' + session
 
+s = requests.Session()
+s.cookies.set('id', core['id'])
+s.cookies.set('token', core['token'])
+
+response = s.get(URL_SESSION, headers=HEADER_SESSION)
+print(response)
+print(response.text)
+print(response.headers)
+if response.status_code != 200:
+    result += "打卡失败！错误：获取session失败" + str(response.status_code)
+    result += response.text
+    
 # Part2 Get header & data
 HEADER = {
     'Accept': 'application/json, text/plain, */*',
@@ -44,22 +53,17 @@ HEADER = {
     'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
     'Connection': 'keep-alive',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Content-length': '1150',
-    'Cookie': cookie,
-    'Host': 'bjut.sanyth.com:81',
-    'Origin': 'http://bjut.sanyth.com:81',
-    'Referer': 'http://bjut.sanyth.com:81/webApp/xuegong/index.html',
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)  Mobile/15E148 wxwork/3.1.18 MicroMessenger/7.0.1 Language/zh ColorScheme/Dark'
-    'Chrome/85.0.4183.83 Safari/537.36',
+    'Origin': 'http://xgxt.bjut.edu.cn',
+    'Referer': 'http://xgxt.bjut.edu.cn/webApp/xuegong/index.html',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; GM1910 Build/RKQ1.201022.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 wxwork/3.1.7 MicroMessenger/7.0.1 NetType/4G Language/zh Lang/zh',
     'X-Requested-With': 'XMLHttpRequest',
 }
 
 # user info
 info = {
     'xmqkb': {
-        'id': '402880c97b1c114b017b1c2af13d02d8'
+        'id': '2c95de297d4f8bfa017d85f53d267613'
     },
-    'c12': core['c12'],
     'c15': core['c15'],
     'c16': core['c16'],
     'c17': core['c17'],
@@ -78,15 +82,17 @@ prefix_raw = 'data='+urllib.parse.quote_plus(prefix_data)
 DATA = prefix_raw + suffix_raw
 
 # Part3 Clock in
-response_clockin = requests.post(url=URL_CLOCKIN, headers=HEADER, data=DATA)
-
-result = '打卡失败'
+response_clockin = s.post(url=URL_CLOCKIN, headers=HEADER, data=DATA)
+print(response_clockin)
+print(response_clockin.text)
+print(response_clockin.headers)
+# result = '打卡失败'
 
 if response_clockin.text == 'success':
-    result = '打卡成功'
+    result += '打卡成功'
 else:
     if response_clockin.text == 'Applied today':
-        result = '今天已经打过卡'
+        result += '今天已经打过卡'
     else:
         result += f'''
 HTTP status: {response_clockin.status_code}
@@ -95,7 +101,7 @@ HTTP status: {response_clockin.status_code}
     json.dumps(info, ensure_ascii=False, sort_keys=True, indent=2)
 }
 '''
-
+print(result)
 message = MIMEText(result, 'plain', 'utf-8')
 message['Subject'] = '打卡结果'
 message['FROM'] = EMAIL_FROM
